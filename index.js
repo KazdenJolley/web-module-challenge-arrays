7eba4a0636fb6c1cbfa1cd86864b71770ac2a4b1
kazden.jolley <kazden.jolley@gmail.com>
2021-04-14 11:34:07 -0600

    completed tasks 5 and 6

--- index.js
+++ index.js
@@ -116,8 +116,8 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(array, index){
+    return array[index];
 }
 
 
@@ -136,8 +136,13 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(array, flavor){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === flavor){
+            array.splice(i, 1);
+        }
+    }
+    return array;
 }
 
 
